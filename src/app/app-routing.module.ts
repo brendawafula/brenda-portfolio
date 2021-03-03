a9971ccd1439c0cae1be912dc9ae81f5b7d3cc85
essy2020 <estherwafula690@gmail.com>
2021-03-03 15:50:00 +0300

    Generating components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { AwardsComponent } from './awards/awards.component';
 import { ContactsComponent } from './contacts/contacts.component';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
 import { HomeComponent } from './home/home.component';
+import { InterestsComponent } from './interests/interests.component';
+import { ProjectsComponent } from './projects/projects.component';
 import { SkillsComponent } from './skills/skills.component';
 
 const routes: Routes = [
@@ -15,8 +20,28 @@ const routes: Routes = [
     component: AboutComponent
   },
   {
+    path: "experience",
+    component: ExperienceComponent
+  },
+  {
+    path: "projects",
+    component: ProjectsComponent
+  },
+  {
+    path: "education",
+    component: EducationComponent
+  },
+  {
     path: "skills",
     component: SkillsComponent
+  },
+  {
+    path: "interests",
+    component: InterestsComponent
+  },
+  {
+    path: "awards",
+    component: AwardsComponent
   },
   {
     path: "contact",
